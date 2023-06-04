20b98b9d7cbed393f03972ea0c5e70333f0cbcfb
Adekunle <kunleoshodi856@gmail.com>
2023-06-04 15:24:56 +0100

    Implemet logout functionality

--- express_server.js
+++ express_server.js
@@ -88,6 +88,13 @@ app.post("/login", (req, res) => {
   res.redirect("/urls/");
 });
 
+// logout route
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+
+  res.redirect("/urls/");
+});
+
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
